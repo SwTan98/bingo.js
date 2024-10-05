7ec8849754bcd242d2439f9b7bce41edb2b4ea29
SW <tansw98@gmail.com>
2024-10-06 01:20:35 +0800

    Add SEO tags

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Alex Bingo",
+  ogTitle: "Alex Bingo",
+  description: "Hello together, Alex prolly thinks this site is OK",
+  ogDescription: "Hello together, Alex prolly thinks this site is OK",
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
