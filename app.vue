3f6a6ac7a44e6e3ffad033b6420ac68f3cff9734
See Wei <17246235+SwTan98@users.noreply.github.com>
2024-10-09 08:04:24 +0000

    update site title

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 useSeoMeta({
-  title: "Alex Bingo",
-  ogTitle: "Alex Bingo",
+  title: "DD Bingo",
+  ogTitle: "DD Bingo",
   description: "Hello together, Alex prolly thinks this site is OK",
   ogDescription: "Hello together, Alex prolly thinks this site is OK",
 });
